fac427b0a3a68fcd19a577a604ff55a7d6c8818a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-07 03:57:54 -0300

    working on moduls 3 metod

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -3,10 +3,18 @@ const sayMyName = (name) => {
 };
 
 sayMyName("marco");
+// 1 metod
 const { lusho, arturito } = require("./02-names");
 sayMyName(lusho);
 sayMyName(arturito);
+// 2 metod
 const names = require("./02-names");
 
 sayMyName(names.eva);
 sayMyName(names.eduardo);
+
+// 3 metod
+
+const randomName = require("./03-alternative-flavor");
+
+console.log(randomName);
