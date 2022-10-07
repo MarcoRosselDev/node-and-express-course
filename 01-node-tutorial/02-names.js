a21c65ab5b6d103704d00c34c17958bc33f4e996
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-07 03:49:50 -0300

    working on moduls 2 metod

--- 01-node-tutorial/02-names.js
+++ 01-node-tutorial/02-names.js
@@ -1,4 +1,7 @@
 const lusho = "lusho";
 const arturito = "arturito";
+// second form of export
+const eva = "eva";
+const eduardo = "eduardo";
 
-module.exports = { lusho, arturito };
+module.exports = { lusho, arturito, eva, eduardo };
--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -6,3 +6,7 @@ sayMyName("marco");
 const { lusho, arturito } = require("./02-names");
 sayMyName(lusho);
 sayMyName(arturito);
+const names = require("./02-names");
+
+sayMyName(names.eva);
+sayMyName(names.eduardo);
