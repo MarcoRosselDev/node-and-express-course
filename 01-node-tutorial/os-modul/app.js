81fc69064da24bcfba95c3bf83071487f4a21dff
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-07 05:08:49 -0300

    user info os.

--- 01-node-tutorial/os-modul/app.js
+++ 01-node-tutorial/os-modul/app.js
@@ -4,3 +4,15 @@ const os = require("os");
 
 const user = os.userInfo();
 console.log(user);
+
+// method return the system uptime in seconds
+
+console.log(`The system Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
