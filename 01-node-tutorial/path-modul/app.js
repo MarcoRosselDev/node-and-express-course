483e4e319dce0cb089b3317bbf107d3d5905b06f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-07 05:33:20 -0300

    path modul 2

--- 01-node-tutorial/path-modul/app.js
+++ 01-node-tutorial/path-modul/app.js
@@ -4,3 +4,10 @@ console.log(path.sep);
 
 const filePath = path.join("/path-modul/", "subfolder", "text.txt");
 console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolute = path.resolve(__dirname, "subfolder", "text.txt");
+
+console.log(absolute);
