43d8b833ea82cd0e0b5b65049dee2d96e292ab10
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-08 18:23:33 -0300

    creating and second url 2

--- 01-node-tutorial/http/app.js
+++ 01-node-tutorial/http/app.js
@@ -7,8 +7,11 @@ const server = http.createServer((req, res) => {
   if (req.url === "/about") {
     res.end("here is our short history");
   }
-  res.write();
-  res.end();
+  res.end(`
+    <h1>Oops !</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="https://www.youtube.com/watch?v=GloCk_25Kj0">link</a>
+  `);
 });
 
 server.listen(5000);
