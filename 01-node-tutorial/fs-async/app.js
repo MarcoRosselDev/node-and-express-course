25bcea6c8fb3c6e37aae7f61418c112c367d339a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-08 15:48:09 -0300

    callback hell 2

--- 01-node-tutorial/fs-async/app.js
+++ 01-node-tutorial/fs-async/app.js
@@ -3,3 +3,13 @@ const { readFileSync, writeFileSync } = require("fs");
 console.log("start");
 const first = readFileSync("../fs-modul/content/first.txt", "utf8");
 const second = readFileSync("../fs-modul/content/second.txt", "utf8");
+
+writeFileSync(
+  "./example.txt",
+  `ejemplo de texto : (${first})
+and : (${second})`,
+  { flag: "a" }
+);
+
+console.log("done with this task");
+console.log("starting the next one");
