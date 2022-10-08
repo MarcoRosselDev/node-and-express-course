3a844a722e3f784eb1f4db502f1b0ef5155c6da4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-08 18:12:11 -0300

    first page XD

--- 01-node-tutorial/http/app.js
+++ 01-node-tutorial/http/app.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.write("welcome to our home page");
+  res.end();
+});
 
 server.listen(5000);
