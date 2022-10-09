a13454dccab1fb6a7ccafc7bf30e0c31586fed36
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-09 07:01:51 -0300

    test package.js

--- 01-node-tutorial/http/app.js
+++ 01-node-tutorial/http/app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   res.end(`
     <h1>Oops !</h1>
     <p>We can't seem to find the page you are looking for</p>
-    <a href="https://www.youtube.com/watch?v=GloCk_25Kj0">link</a>
+    <a href="https://www.youtube.com/watch?v=TNV0_7QRDwY&t=5370s>link</a>
   `);
 });
 
