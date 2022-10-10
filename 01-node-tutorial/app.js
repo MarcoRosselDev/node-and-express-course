c7e299a20129bbf5ce8a7d8e5a7d98825e2aa47e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-10 08:45:16 -0300

    no comprendo ahun muy bien como funcionan los folders para llamarlos

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,9 +1,13 @@
 const { readFile } = require("fs");
 
-readFile("./fs-async/content/first.txt", "utf8", (err, data) => {
-  if (err) {
-    return;
-  } else {
-    console.log(data);
+readFile(
+  "./01-node-tutorial/fs-modul/content/first.txt",
+  "utf8",
+  (err, data) => {
+    if (err) {
+      return;
+    } else {
+      console.log(data);
+    }
   }
-});
+);
