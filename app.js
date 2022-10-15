e05d859a21f4da3770f11c4d823e518242c629cf
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 18:08:38 -0300

    port 5000 in use? how closed?

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.listen(4999, () => {
+  console.log("lisining");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
