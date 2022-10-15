451f8d238d4283bddc845d88fd4e65e98e7ae771
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 14:55:16 -0300

    express tutorial 1

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("express tutorial");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end('home page, para que no se confunda el navegador')
+});
+
+server.listen(5000);
