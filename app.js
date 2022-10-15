f034b88e807a37c812daacb7e096f17a8359f9ca
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 15:38:39 -0300

    html response

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
-  res.end('home page, para que no se confunda el navegador')
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>home page</h1>");
+  res.end("home page, para que no se confunda el navegador");
 });
 
 server.listen(5000);
