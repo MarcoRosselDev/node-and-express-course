e6725618e8c0eb679df1503ebe03b18723d55188
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 17:04:19 -0300

    all is good and clean

--- app.js
+++ app.js
@@ -3,14 +3,38 @@ const { readFileSync } = require("fs");
 
 // get all files
 const homePage = readFileSync("./02-express-tutorial/navbar-app/index.html");
+const homeStyles = readFileSync("./02-express-tutorial/navbar-app/styles.css");
+const homeImage = readFileSync("./02-express-tutorial/navbar-app/logo.svg");
+const homeLogic = readFileSync(
+  "./02-express-tutorial/navbar-app/browser-app.js"
+);
 
 const server = http.createServer((req, res) => {
+  console.log("added");
   const url = req.url;
 
   // home page
   if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
+    res.end();
+  }
+  // home styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // home logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  }
+  // home image
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
     res.end();
   }
 
