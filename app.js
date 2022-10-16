7250c277f98ccca56a2eeeb86f9d977f44bb9e72
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 22:56:08 -0300

    express ref

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.static("./02-express-tutorial/public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(
-//     path.resolve(__dirname, "./02-express-tutorial/navbar-app/index.html")
-//   );
-// });
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+app.get("/", (req, res) => {
+  res.json();
 });
 
 app.listen(5000, () => {
-  console.log("lisining");
+  console.log("server is listening on port 5000...");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
