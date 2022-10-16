8d7638200c23fb8cf0afe847fc53076356f3da76
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-15 22:11:15 -0300

    podemos incluir el html en public folder

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(express.static("./02-express-tutorial/public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "./02-express-tutorial/navbar-app/index.html")
-  );
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(
+//     path.resolve(__dirname, "./02-express-tutorial/navbar-app/index.html")
+//   );
+// });
 app.all("*", (req, res) => {
   res.status(404).send("<h1>resource not found</h1>");
 });
