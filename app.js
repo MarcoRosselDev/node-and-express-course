8ce6eea2d5747eda7d037944a11e3e55864d365b
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-16 23:24:32 -0300

    a lot of thinks apends

--- app.js
+++ app.js
@@ -30,7 +30,22 @@ app.get("/api/products/:productID", (req, res) => {
 });
 
 app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
+  // console.log(req.query);
+
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  res.status(200).json(sortedProducts);
+
   res.send("hola");
 });
 
