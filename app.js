5e78f1c83fdda8eb2b8dd107ecea6b59dc37069a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-16 22:07:36 -0300

    get products .map

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send('<h1>home page</h1><a href="/api/products">products</a>');
 });
 
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
 });
