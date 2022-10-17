b2331a751542147d10fdc52f318865b24cef2830
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-17 14:37:38 -0300

    return an object if not search nothing

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.get("/api/v1/query", (req, res) => {
     sortedProducts = sortedProducts.slice(0, Number(limit));
   }
   if (sortedProducts.length < 1) {
-    res.status(200).send("no products matched your search");
+    //    res.status(200).send("no products matched your search");
+    return res.status(200).json({ succes: true, data: [] });
   }
   res.status(200).json(sortedProducts);
 });
