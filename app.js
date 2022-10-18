39e5ebff7270bd8ad71344a3bf2a4597c3482f9f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-18 00:15:20 -0300

    post on /login

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/login", (req, res) => {
+  res.send("POST");
+});
+
 app.listen(5000, () => {
   console.log("server listening on port 5000...");
 });
