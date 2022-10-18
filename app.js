dbccb71de14b7f8efe76ff39a19603bd9a54c0b8
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-18 01:03:33 -0300

    post on /login succesfull axios

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+  res.status(201).send("success");
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
