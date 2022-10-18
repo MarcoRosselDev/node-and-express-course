31b4acb900194a80ee1bb7aa8a3c3ec6dafb6639
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-17 23:53:16 -0300

    get tutorial

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const logger = require("./logger");
-const authorize = require("./authorize");
-// req => middleware => res
+let { people } = require("./data");
 
-//app.use([logger, authorize]);
-//app.use(express.static("./02-express-tutorial/public"));
-
-app.use(morgan("tiny"));
-
-app.get("/", (req, res) => {
-  res.send("home");
-});
-app.get("/about", (req, res) => {
-  res.send("about");
-});
-app.get("/api/products", (req, res) => {
-  res.send("products");
-});
-app.get("/api/items", (req, res) => {
-  res.send("items");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
-  console.log("server listening on port 5000");
+  console.log("server listening on port 5000...");
 });
