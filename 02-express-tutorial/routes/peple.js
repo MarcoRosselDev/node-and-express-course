15149c4326e2ce75592bff3022188f1338df5a70
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-19 01:38:35 -0300

    router api

--- 02-express-tutorial/routes/peple.js
+++ 02-express-tutorial/routes/peple.js
@@ -1,4 +1,64 @@
 const express = require("express");
 const reuter = express.Router();
 
-https://www.wonder.legal/cl/creation-modele/carta-renuncia-voluntaria-cl-6
+let { people } = require("./data");
+
+router.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+router.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ succes: false, msg: "please provide name value" });
+  }
+  res.status(201).send({ success: true, person: name });
+});
+
+router.post("/api/people/postman", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "pelase provide name value <3" });
+  }
+  res.status(201).send({ succes: true, data: [...people, name] });
+});
+
+router.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+router.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
+module.exports = router;
